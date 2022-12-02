fe45f002456f8b680ef0983bdd68fb99240326b8
ealvarez94 <94322109+ealvarez94@users.noreply.github.com>
2022-12-02 19:43:51 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <title>Proyecto P6</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<header>
+			<a class="a" href="index.php">Inicio</a>
+	</header>
